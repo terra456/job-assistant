e883ed631301e29ff5355c57fb6e3e1bb25e5247
Olga Baynova (terra456) <terra.456@yandex.ru>
2025-06-12 21:48:05 +0500

    feat: bigin api

---
+++ src/lib/definitions.ts
@@ -0,0 +1,18 @@
+export interface Vacancy {
+    id: number,
+    active: boolean,
+    external_id: string,
+    company_name: string,
+    title: string,
+    salary: string,
+    location: string,
+    speciality: string,
+    internship: boolean,
+    remote: boolean,
+    url: string,
+    description: string,
+    source: string,
+    image: string,
+    date_publication: string,
+  }
+
