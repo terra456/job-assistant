d005ccb7f68c634e24512100bf006a9da9a4c901
Olga Bainova <52218799+terra456@users.noreply.github.com>
2025-06-16 20:49:27 +0500

    Update daa vacancie page.tsx
    For deploying

--- src/app/vacancies-and-internships/add/page.tsx
+++ src/app/vacancies-and-internships/add/page.tsx
@@ -1,11 +1,3 @@
-export async function generateStaticParams() {
-  const posts = await fetch("https://.../posts").then((res) => res.json());
-
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
 export default function Page() {
   return <p>Dashboard Page</p>;
 }
