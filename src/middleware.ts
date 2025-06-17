08eab3443717716848c53c1a875a0049bf730b0f
Olga Baynova (terra456) <terra.456@yandex.ru>
2025-06-17 20:55:23 +0500

    feat: set cookie for specialisation

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,22 @@ import { stack } from './lib/constants'
  
 export function middleware(request: NextRequest) {;
   let cookie = request.cookies.get('speciality');
-  console.log(cookie);
+  console.log('cookie', cookie);
   const currentSearchParams = request.url;
   console.log('currentSearchParams', currentSearchParams);
   const searchParams = request.nextUrl.searchParams.get('speciality');
   console.log('searchParams', searchParams);
 
+  if (!cookie && searchParams) {
+    if (searchParams && stack.has(searchParams)) {
+      const response = NextResponse.next();
+      response.cookies.set('speciality', searchParams);
+  
+      cookie = response.cookies.get('speciality');
+      console.log(cookie);
+      return response;
+    }
+  }
   
   if (cookie && !searchParams) {
     const url = request.nextUrl.clone();
